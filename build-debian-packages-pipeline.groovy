7d4147e72ccdc68c85babe16ca4bc93815d35e6d
Oleg Iurchenko <oiurchenko@mirantis.com>
2017-11-23 15:45:07 +0200

    tttt

--- build-debian-packages-pipeline.groovy
+++ build-debian-packages-pipeline.groovy
@@ -168,7 +168,7 @@ node("docker") {
     }
     if (deployOS) {
       stage("Deploy OpenStack with changed package") {
-        deployBuild = build (job: oiurchenko_aio_test, propagate: false,
+        deployBuild = build (job: 'oiurchenko_aio_test', propagate: false,
           parameters: [
             [$class: 'StringParameterValue', name: 'FORMULA_PKG_REVISION', value: 'stable'],
             [$class: 'StringParameterValue', name: 'HEAT_STACK_ENVIRONMENT', value: 'devcloud'],
