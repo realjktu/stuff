a8351fbba7b06bce86f1419687184de59dfd98b9
Oleg Iurchenko <oiurchenko@mirantis.com>
2017-12-11 16:10:23 +0200

    test

--- cloud-deploy-pipeline.groovy
+++ cloud-deploy-pipeline.groovy
@@ -276,7 +276,7 @@ node(slave_node) {
                 def master = venvPepper
                 if (salt.testTarget(master, 'I@rabbitmq:server') && salt.testTarget(master, 'I@baremetal_simulator:enabled')) {                    
                     salt.cmdRun(master, 'cfg01*', 'salt -C "I@rabbitmq:server" test.ping | grep ":" | xargs -I{} scp {}/tmp/test_source /tmp/', false)
-                    salt.cmdRun(master, 'cfg01*', 'salt -C "I@baremetal_simulator:enabled" test.ping | grep ":" | xargs -I{} scp /tmp/test_source {}/tmp/', false)
+                    salt.cmdRun(master, 'cfg01*', 'salt -C "I@baremetal_simulator:enabled" test.ping | grep ":" | xargs -I{} scp /tmp/test_source {}/tmp/')
                     //salt.cmdRun(master, 'cfg01*', 'rm -f /tmp/test_source')
                 }
 
